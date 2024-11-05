43f051530843c5c979b76b462dfb6d6d1b5e488d
drewbieboobies <2302416I@student.tp.edu.sg>
2024-11-06 01:33:58 +0800

    Resolved merge conflicts in index.js and utils/students.json

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var bodyParser = require("body-parser");
-<<<<<<< Updated upstream
-=======
+
 const { updateStudent } = require('./utils/updateStudent');
->>>>>>> Stashed changes
+
 var app = express();
 
 const PORT = process.env.PORT || 5050;
@@ -19,12 +18,9 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/" + startPage);
 })
 
-<<<<<<< Updated upstream
-=======
+
 app.put('/update-student/:id', updateStudent);
 
-
->>>>>>> Stashed changes
 server = app.listen(PORT, function () {
     const address = server.address();
     const baseUrl = `http://${address.address == "::" ? 'localhost' :
