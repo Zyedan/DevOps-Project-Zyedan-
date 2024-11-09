64bc8b2e17f9a0573b02450ddae8a9d5577f9b4a
Zyedan <2300041G@student.tp.edu.sg>
2024-11-09 22:58:02 +0800

    Tested and reviewed both add-course and update-course

--- utils/addCourseUtil.js
+++ utils/addCourseUtil.js
@@ -7,23 +7,23 @@ const fs = require('fs').promises;
 
 // Function to read JSON file
 async function readJSON(filename) {
-	try {
-		const data = await fs.readFile(filename, 'utf8');
-		return JSON.parse(data);
-	} catch (err) {
-		console.error(err); throw err;
-	}
+    try {
+        const data = await fs.readFile(filename, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error(err); throw err;
+    }
 }
 
 // function to write JSON file
 async function writeJSON(object, filename) {
-	try {
-		const allObjects = await readJSON(filename);
-		allObjects.push(object);
+    try {
+        const allObjects = await readJSON(filename);
+        allObjects.push(object);
 
-		await fs.writeFile(filename, JSON.stringify(allObjects), 'utf8');
-		return allObjects;
-	} catch (err) { console.error(err); throw err; }
+        await fs.writeFile(filename, JSON.stringify(allObjects), 'utf8');
+        return allObjects;
+    } catch (err) { console.error(err); throw err; }
 }
 
 // Function to add a course
@@ -34,14 +34,14 @@ async function addCourse(req, res) {
         const description = req.body.description;
         let modules = req.body.modules;
         if (!Array.isArray(modules)) {
-            modules = [modules]; 
+            modules = [modules];
         }
         const course_department = req.body.course_department;
         const course_fee = req.body.course_fee;
         const requirements = req.body.requirements;
         const course_intake = req.body.course_intake;
 
-		// Validate the course details
+        // Validate the course details
         if (!/^\d+(\.\d+)?$/.test(course_fee) || !/^\d+$/.test(course_intake)) {
             return res.status(400).json({ message: 'Validation error' });
         } else { // Create a new course object
@@ -53,5 +53,5 @@ async function addCourse(req, res) {
         return res.status(500).json({ message: error.message });
     }
 }
-
+//Do not remove this line
 module.exports = { readJSON, writeJSON, addCourse };
